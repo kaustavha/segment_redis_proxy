10ee0084bf675b629f7deca8eb6f69e22d7cf344
Kaustav Haldar <hi@kaustav.me>
2017-12-09 13:54:08 -0500

    patch parse key

--- lru_proxy_resp.go
+++ lru_proxy_resp.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"net"
 	"bufio"
 	"fmt"
+	"net"
 	"strings"
 )
 
 type RedisProxyTCP struct {
-	cache  *TimedLRUCache
-	ln net.Listener
+	cache   *TimedLRUCache
+	ln      net.Listener
 	running bool
-	addr string
-	sem chan struct{}
+	addr    string
+	sem     chan struct{}
 }
 
 func NewRESPProxy(redis_addr string, expiry_time int, capacity int, listen_proxy string, maxClients int) *RedisProxyTCP {
 	cache := NewCache(expiry_time, capacity)
 	ln, _ := net.Listen("tcp", listen_proxy)
 	rptcp := &RedisProxyTCP{
-		cache: cache,
-		ln: ln,
-		addr: redis_addr,
+		cache:   cache,
+		ln:      ln,
+		addr:    redis_addr,
 		running: true,
-		sem: make(chan struct{}, maxClients),
+		sem:     make(chan struct{}, maxClients),
 	}
 	rptcp.cache.FetchMissing = func(key interface{}) (interface{}, bool) {
 		return rptcp.GetFromBR(key)
@@ -39,12 +39,11 @@ func (rptcp *RedisProxyTCP) Close() {
 
 func (rptcp *RedisProxyTCP) Open() (*bufio.ReadWriter, error) {
 	conn, err := net.Dial("tcp", rptcp.addr)
-	if (err != nil) {
+	if err != nil {
 		return nil, err
 	}
 	return bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn)), nil
 }
-
 
 func (rptcp *RedisProxyTCP) Listen() {
 
@@ -53,18 +52,18 @@ func (rptcp *RedisProxyTCP) Listen() {
 		if !rptcp.running {
 			return
 		}
-		rptcp.sem<-struct{}{}
-	    message, err := bufio.NewReader(conn).ReadString('\n')
-	    if err != nil {
-	    	<-rptcp.sem
-	    }
+		rptcp.sem <- struct{}{}
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			<-rptcp.sem
+		}
 		go func() {
 
-		    resp := rptcp.Proxy(message)
+			resp := rptcp.Proxy(message)
 
-		    <-rptcp.sem
+			<-rptcp.sem
 
-		    conn.Write([]byte(resp+"\n"))
+			conn.Write([]byte(resp + "\n"))
 		}()
 
 	}
@@ -86,7 +85,7 @@ func (rptcp *RedisProxyTCP) Proxy(str string) string {
 func (rptcp *RedisProxyTCP) GetFromBR(key interface{}) (interface{}, bool) {
 	str := fmt.Sprint(key)
 	rw, _ := rptcp.Open()
-	cmdStr := "*2\r\n$3\r\nGET\r\n$"+fmt.Sprint(len(str))+"\r\n"+str
+	cmdStr := "*2\r\n$3\r\nGET\r\n$" + fmt.Sprint(len(str)) + "\r\n" + str
 	rw.WriteString(cmdStr)
 	rw.Flush()
 	resp, err := rw.ReadString('\n')
@@ -98,7 +97,9 @@ func (rptcp *RedisProxyTCP) GetFromBR(key interface{}) (interface{}, bool) {
 
 func (rptcp *RedisProxyTCP) ParseKey(str string) (bool, string) {
 	words := strings.Fields(str)
-	if strings.ToUpper(words[0]) == "GET" {
+	if strings.ToUpper(words[3]) == "GET" {
+		return true, strings.Fields(str)[4]
+	} else if strings.ToUpper(words[0]) == "GET" {
 		return true, strings.Fields(str)[1]
 	} else {
 		return false, ""
